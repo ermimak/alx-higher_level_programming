2c0f9d89634bd2bb264d2d8255bcd2f41c3a0a67
Ermias Mekonnen <87018596+ermimak@users.noreply.github.com>
2022-08-17 09:13:48 +0300

    after long time

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+File: 6-base_geometry.py
+Desc: This module defines an empty class.
+Author: Assel
+Date Created: Aug 1 2022
+"""
+
+
+class BaseGeometry:
+    """
+    A simple class defination.
+    """
+    def area(self):
+        """
+        A function that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
